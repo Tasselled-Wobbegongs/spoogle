1bda03df15ed193c5e6eb972b6c1ba8387ba5dc4
Katrina Villanueva <klgvillanueva@gmail.com>
2021-01-16 14:17:58 -0500

    working on changing dropdowns to sliders

---
+++ client/components/Dropdown.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const dropdown = (props) => {
+    
+
+    // <div className="dropdown" style={{float: "left"}}>
+    //   <button className="dropbtn">{props.dropdownName}</button>
+    //   <div className="dropdown-content" style={{left:0}}>
+    //     <a href="#">20-50 BPM</a>
+    //     <a href="#">50-80 BPM</a>
+    //     <a href="#">80-120 BPM</a>
+    //     <a href="#">120-170 BPM</a>
+    //     <a href="#">170-200 BPM</a>
+    //   </div>
+    // </div>
+    // <div className="slidecontainer">
+    // <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+    // <p>Value: <span id="demo"></span></p>
+    // </div>
+    // <script>
+    // var slider = document.getElementById("myRange");
+    // var output = document.getElementById("demo");
+    // output.innerHTML = slider.value;
+    
+    // slider.oninput = function() {
+    //   output.innerHTML = this.value;
+    // }
+    // </script>
+  // )
+
+
+}
+
+
+
+export default dropdown;
--- client/components/SearchBar.jsx
+++ client/components/SearchBar.jsx
@@ -1,30 +1,130 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import Dropdown from './Dropdown.jsx'
 
-const SearchBar = ({handleSubmit}) => {
-  return (
-    <form onSubmit={handleSubmit}>
-      <input name='artist' type='text'></input> 
-      <input type='submit' value='spoogle'></input> 
-    </form>
-  )
 
-const TempoDropDown = () => {
-  return (
-    <div className="dropdown">
-      <button className="dropbtn">Tempo</button>
+// const handleDropdown = (e) => {
+//   e.preventDefault();
+//   /* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+//   document.getElementById("tempoDropdown").classList.toggle("show");
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(e) {
+//     if (!e.target.matches('.dropbtn')) {
+//       var tempDropdown = document.getElementById("tempoDropdown");
+//     if (tempoDropdown.classList.contains('show')) {
+//       tempoDropdown.classList.remove('show');
+//       }
+//     }
+//   }
+//   // send search params to server
+//   // server responds with array of tracks, "data"
+// }
+
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchParameters : ["target_danceability", "target_acousticness", "target_popularity", "target_valence", "target_tempo", "target_duration_ms"]
+    }
+  };
+
+  render() {
+    const dropdowns = [];
+    
+    for (let i = 0; i < this.state.searchParameters.length; i += 1) {
+      dropdowns.push(<Dropdown dropdownName={this.state.searchParameters[i]}/>);
+    }
+
+    return (
+      <Fragment>
+      <form onSubmit={this.props.handleSubmit}>
+        <input name='artist' type='text'></input> 
+        <input type='submit' value='spoogle'></input> 
+      </form>
+      {dropdowns}
+      {/* <div className="dropdown" style={{float: "left"}}>
+        <button className="dropbtn">Danceability</button>
+        <div className="dropdown-content" style={{left:0}}>
+          <a href="#">Still as a Rock</a>
+          <a href="#">Give a Little Sway</a>
+          <a href="#"></a>
+          <a href="#">Grooving Out</a>
+          <a href="#">Spaz Out</a>
+        </div>
+      </div> */}
+      <div className="dropdown" style={{float: "left"}}>
+        <button className="dropbtn">Duration</button>
+        <div className="dropdown-content" style={{left:0}}>
+          <a href="#">20-50 BPM</a>
+          <a href="#">50-80 BPM</a>
+          <a href="#">80-120 BPM</a>
+          <a href="#">120-170 BPM</a>
+          <a href="#">170-200 BPM</a>
+        </div>
+      </div>
+      <div className="dropdown" style={{float: "left"}}>
+        <button className="dropbtn">Popularity</button>
+        <div className="dropdown-content" style={{left:0}}>
+          <a href="#">20-50 BPM</a>
+          <a href="#">50-80 BPM</a>
+          <a href="#">80-120 BPM</a>
+          <a href="#">120-170 BPM</a>
+          <a href="#">170-200 BPM</a>
+        </div>
+      </div>
+      <div className="dropdown" style={{float: "left"}}>
+        <button className="dropbtn">Tempo</button>
+        <div className="dropdown-content" style={{left:0}}>
+          <a href="#">20-50 BPM</a>
+          <a href="#">50-80 BPM</a>
+          <a href="#">80-120 BPM</a>
+          <a href="#">120-170 BPM</a>
+          <a href="#">170-200 BPM</a>
+        </div>
+      </div>
+      <div className="dropdown" style={{float: "left"}}>
+        <button className="dropbtn">Valence</button>
+        <div className="dropdown-content" style={{left:0}}>
+          <a href="#">20-50 BPM</a>
+          <a href="#">50-80 BPM</a>
+          <a href="#">80-120 BPM</a>
+          <a href="#">120-170 BPM</a>
+          <a href="#">170-200 BPM</a>
+        </div>
+      </div>
+      </Fragment>
+    )
+  }
+}
+
+
+    {/* <div className="dropdown">
+      <button className="dropbtn" onClick={handleDropdown}>Tempo</button>
       <div id="tempoDropdown" className="dropdown-content">
         <a href="#">20-50 BPM</a>
         <a href="#">50-80 BPM</a>
         <a href="#">80-120 BPM</a>
         <a href="#">120-170 BPM</a>
         <a href="#">170-200 BPM</a>
-    </div>
+      </div>  
+    </div> */}
 
 
-  )
-}  
+// const TempoDropDown = () => {
+//   return (
+//     <div className="dropdown">
+//       <button className="dropbtn" onClick={handleDropdown}>Tempo</button>
+//       <div id="tempoDropdown" className="dropdown-content">
+//         <a href="#">20-50 BPM</a>
+//         <a href="#">50-80 BPM</a>
+//         <a href="#">80-120 BPM</a>
+//         <a href="#">120-170 BPM</a>
+//         <a href="#">170-200 BPM</a>
+//       </div>  
+//     </div>
+//   )
+// }  
 
-}
 
 export default SearchBar;
