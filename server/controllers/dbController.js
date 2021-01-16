f01a4b79275124eb7a8ed08d90d992b9805e604c
Garrett Weaver <uncommonweaver@gmail.com>
2021-01-15 18:06:15 -0600

    pulling all users from db

---
+++ server/controllers/dbController.js
@@ -0,0 +1,36 @@
+// allowing access to query which is exported in 'models/dbModels.js'
+const db = require('../models/dbModels.js');
+
+// defining an empty object literal, will be populated while this file executes, then exported
+const dbController = {};
+
+// capture everything currently in the user table (nonexistent at the moment)
+dbController.allUsers = (req, res, next) => {
+
+    // query string to select everything from user table
+    const getAllQuery = 'SELECT * FROM user'
+
+    // submitting the query string to the database
+    db.query(getAllQuery)
+
+    // if successful, capture the db specific response
+    .then(response => {
+        next()
+    })
+
+    // if unsuccessful, make me aware of any error
+    .catch(err => {
+        return next({
+            // logging a string
+            log: `Database error`,
+            // error status
+            status: 502,
+            // posting which middleware returned an error along with the error itself
+            message: { err: `dbController.allUsers, ${error.stack}` }
+        })
+    })
+}
+
+// exports the completed dbController object
+// dbController will be used in api.js
+module.exports = dbController;
---
+++ server/routes/apiDB.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const dbController = require('../controllers/apiDB');
+
+// respond with every user currently in database
+router.get('/', 
+    usedCarController.allUsers,
+    (req, res) => res.status(200).json({})
+)
+
+// exports the currently used routers under the
+module.exports = router;
