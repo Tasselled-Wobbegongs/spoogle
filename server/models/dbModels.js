305f35670d763b3811b6956f967cb572e727a0a7
Garrett Weaver <uncommonweaver@gmail.com>
2021-01-16 21:13:35 -0600

    fully functioning CRUD communication to database

--- server/models/dbModels.js
+++ server/models/dbModels.js
@@ -15,7 +15,7 @@ const pool = new Pool({
 module.exports = {
     query: (text, params, callback) => {
         // will log which query string was executed
-        console.log(`executed query: ${text}`)
+        console.log(`Executed query: ${text}`)
         return pool.query(text, params, callback)
     }
 }
--- server/routes/apiDB.js
+++ server/routes/apiDB.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
-
+// allowing access to dbController which is at ../controllers/dbController
 const dbController = require('../controllers/dbController');
 
-// respond with every user currently in database
-router.get('/', 
-    dbController.allUsers,
-    (req, res) => res.status(200).json(res.locals.all)
+// submits a new user to the database
+router.post('/:id',
+    dbController.createUser,
+    (req, res) => res.sendStatus(200)
 )
 
-// exports the currently used routers under the
+// responds with every user currently in database
+router.get('/', 
+    dbController.allUsers, 
+    (req, res) => res.status(200).json(res.locals.allUsers)
+)
+
+// responds with the requested user
+router.get('/getUser/:id',
+    dbController.getUser,
+    (req, res) => res.status(200).json(res.locals.user)
+)
+
+// updates the current favorites stored in the database for a particular user
+router.patch('/updateFav/:id', 
+    dbController.updateFavorites, 
+    (req, res) => res.sendStatus(200)
+)
+
+// deletes a specific user
+router.delete('/:id',
+    dbController.deleteUser,
+    (req, res) => res.sendStatus(200)
+)
+
+// exports the routers used in a single cycle
 module.exports = router;
