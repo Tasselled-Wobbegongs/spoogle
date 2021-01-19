ebe995f955e9b5a6e8116bfdc3da1bf8510a16d5
klgvillanueva <69995214+klgvillanueva@users.noreply.github.com>
2021-01-18 23:16:29 -0500

    Update Filters.jsx
    changed to props.id

--- client/components/Filters.jsx
+++ client/components/Filters.jsx
@@ -20,7 +20,7 @@ function Filters(props) {
   const [value, setValue] = React.useState([props.parameterObj.min, props.parameterObj.max]);
 
   const handleChange = (event, newValue) => {
-    props.onChangeFunc(event, newValue)
+    props.onChangeFunc(props.id, newValue)
     setValue(newValue);
   };
   return (
